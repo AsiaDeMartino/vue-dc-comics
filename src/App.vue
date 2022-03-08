53b232da1a8069b7e694134e587b5ffcf23a7e05
asia_demartino@libero.it <asia_demartino@libero.it>
2022-03-08 22:02:43 +0100

    aggiunto componente Main

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-  
+   <MyHeader />
+   <MyMain />
     
   </div>
 </template>
 
 <script>
-//  import MyHeader from './components/MyHeader.vue'
+ import MyHeader from './components/MyHeader.vue'
+import MyMain from '/src/components/MyMain.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    MyHeader,
+    MyMain,
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   color: black;
 }
---
+++ src/components/MyMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <h1> content goes here </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+}
+</script>
+
+
+<style scoped>
+.main{
+    color: white;
+    background-color: black;
+    height: 100%;
+    padding: 50px;
+}
+</style>
